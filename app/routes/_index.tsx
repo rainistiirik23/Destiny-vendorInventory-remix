f053fff817069e8977f3f44c1c3e4b63c24ce9a2
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-03 11:20:59 +0300

    Homepage route for vendor sales

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,14 @@
-export default function homePage() {
-  return <div>Home</div>;
+/* import { DataFunctionArgs } from "@remix-run/node"; */
+import { useLoaderData } from "@remix-run/react";
+import { loadVendorData } from "~/loaders/loadVendorData";
+import CurrentVendorInventoryPage from "~/pages/vendorSales";
+export async function loader() {
+  const vendorData = loadVendorData();
+  return vendorData;
+}
+
+export default function VendorWishlist() {
+  const vendorData = useLoaderData<typeof loader>();
+
+  return <CurrentVendorInventoryPage {...vendorData}></CurrentVendorInventoryPage>;
 }
