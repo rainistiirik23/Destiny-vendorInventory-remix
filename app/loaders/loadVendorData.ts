97656a05215fa33c103904da6690d992d8b06e2c
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-08 14:41:48 +0300

    Added seperate laoders for all vendor sales and current ones

--- app/loaders/loadVendorData.ts
+++ app/loaders/loadVendorData.ts
@@ -1,11 +1,15 @@
-import { getCurrentAndAllVendorSales, getDiscordAccountId } from "~/utils/requests";
+import { getCurrentVendorSales, getDiscordAccountId,getAllVendorSales } from "~/utils/requests";
 
-export async function loadVendorData() {
-    const vendorData = await getCurrentAndAllVendorSales()
+export async function loadCurrentVendorSales() {
+    const vendorData = await getCurrentVendorSales()
     return {data:{vendorData}}
 }
 export async function loadVendorDataWithDiscordId(code:string) {
     const vendorData = await getCurrentAndAllVendorSales();
     const discordAccountId = await getDiscordAccountId(code);
     return {data:{vendorData,discordAccountId}}
+}
+export async function loadAllVendorSales(){
+const vendorData = await getAllVendorSales();
+ return {data:{vendorData}}
 }
