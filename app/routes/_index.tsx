a2d8dbcb968418749a42a43a1b4ab4bc2ec0bd1f
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-17 18:45:00 +0300

    Get current vendor sales

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,15 @@
 /* import { DataFunctionArgs } from "@remix-run/node"; */
 import { useLoaderData } from "@remix-run/react";
-import { loadVendorData } from "~/loaders/loadVendorData";
+import { loadCurrentVendorSales } from "~/loaders/loadVendorData";
 import CurrentVendorInventoryPage from "~/pages/vendorSales";
 export async function loader() {
-  const vendorData = loadVendorData();
+  const vendorData = loadCurrentVendorSales();
   return vendorData;
 }
 
 export default function VendorWishlist() {
   const vendorData = useLoaderData<typeof loader>();
+  console.log(vendorData);
 
   return <CurrentVendorInventoryPage {...vendorData}></CurrentVendorInventoryPage>;
 }
