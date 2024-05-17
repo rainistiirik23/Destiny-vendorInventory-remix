5fea066725632e3e92025cfe686ffa9ef3046781
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-17 18:42:00 +0300

    Get all vendor's data with discord id and users wishlisted items

--- app/loaders/loadVendorData.ts
+++ app/loaders/loadVendorData.ts
@@ -1,15 +1,20 @@
-import { getCurrentVendorSales, getDiscordAccountId,getAllVendorSales } from "~/utils/requests";
+import { getCurrentVendorSales, getDiscordAccountId,getAllVendorSales,getVendorSalesWithUsersWishListedSales } from "~/utils/requests";
 
 export async function loadCurrentVendorSales() {
     const vendorData = await getCurrentVendorSales()
     return {data:{vendorData}}
 }
 export async function loadVendorDataWithDiscordId(code:string) {
-    const vendorData = await getCurrentVendorSales();
+    const vendorData = await getAllVendorSales();
     const discordAccountId = await getDiscordAccountId(code);
     return {data:{vendorData,discordAccountId}}
 }
 export async function loadAllVendorSales(){
 const vendorData = await getAllVendorSales();
  return {data:{vendorData}}
+}
+export async function loadAllVendorSalesWithUsersWishListedSales(userID:unknown){
+const usersWishListedSales = await getVendorSalesWithUsersWishListedSales(userID);
+const vendorData = await getAllVendorSales();
+return{data:{vendorData,usersWishListedSales}}
 }
