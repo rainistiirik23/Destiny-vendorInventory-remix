bb72e381c4ad50a5230e32c249eeecca2077fc93
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-09 10:26:28 +0300

    Changed elements,added links and class names

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,19 +4,54 @@ export default function Navbar(data: unknown) {
   if (data.userData) {
     const userData = data.userData.showData;
     return (
-      <nav>
-        <form action="/vendorWishList" method="POST">
-          <button>Log out</button>
-        </form>
-        <span>Logged in as {userData.global_name}</span>
-      </nav>
+      <div className="navigation-links-container">
+        <nav>
+          <ul className="navigation-links-unordered-list">
+            <li>
+              <a className="navigation-link" href="/vendorWishList">
+                Vendor WishList
+              </a>
+            </li>
+            <li>
+              <a className="navigation-link" href="/">
+                All vendor sales
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div className="log-out-form-container">
+          <form action="/vendorWishList" method="POST">
+            <button>Log out</button>
+          </form>
+          <span>Logged in as {userData.global_name}</span>
+        </div>
+      </div>
     );
   }
   return (
-    <nav>
-      <a href="https://discord.com/oauth2/authorize?client_id=1231201012258177054&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A5173%2FvendorWishList&scope=identify">
-        Login with discord
-      </a>
-    </nav>
+    <div className="navigation-links-container">
+      <nav>
+        <ul className="navigation-links-unordered-list">
+          <li>
+            <a className="navigation-link" href="/vendorWishList">
+              Vendor WishList
+            </a>
+          </li>
+          <li>
+            <a className="navigation-link" href="/">
+              All vendor sales
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <a
+          className="navigation-link"
+          href="https://discord.com/oauth2/authorize?client_id=1231201012258177054&response_type=code&redirect_uri=http%3A%2F%2F127.0.0.1%3A5173%2FvendorWishList&scope=identify"
+        >
+          Login with discord
+        </a>
+      </div>
+    </div>
   );
 }
