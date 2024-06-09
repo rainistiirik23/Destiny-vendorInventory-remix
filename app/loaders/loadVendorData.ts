b2c7738976aaca4c88510e3ed502143c35fa3768
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-09 10:25:19 +0300

    Added load current vendor sales with discord id loader

--- app/loaders/loadVendorData.ts
+++ app/loaders/loadVendorData.ts
@@ -1,20 +1,29 @@
-import { getCurrentVendorSales, getDiscordAccountId,getAllVendorSales,getVendorSalesWithUsersWishListedSales } from "~/utils/requests";
+import {
+  getCurrentVendorSales,
+  getDiscordAccountId,
+  getAllVendorSales,
+  getVendorSalesWithUsersWishListedSales,
+} from "~/utils/requests";
 
 export async function loadCurrentVendorSales() {
-    const vendorData = await getCurrentVendorSales()
-    return {data:{vendorData}}
+  const vendorData = await getCurrentVendorSales();
+  return { vendorData };
 }
-export async function loadVendorDataWithDiscordId(code:string) {
-    const vendorData = await getAllVendorSales();
-    const discordAccountId = await getDiscordAccountId(code);
-    return {data:{vendorData,discordAccountId}}
+export async function loadCurrentVendorSalesWithDiscordId(code: string) {
+  const discordAccountId = await getDiscordAccountId(code);
+  return { data: { discordAccountId } };
 }
-export async function loadAllVendorSales(){
-const vendorData = await getAllVendorSales();
- return {data:{vendorData}}
+export async function loadVendorDataWithDiscordId(code: string) {
+  const vendorData = await getAllVendorSales();
+  const discordAccountId = await getDiscordAccountId(code);
+  return { data: { vendorData, discordAccountId } };
 }
-export async function loadAllVendorSalesWithUsersWishListedSales(userID:unknown){
-const usersWishListedSales = await getVendorSalesWithUsersWishListedSales(userID);
-const vendorData = await getAllVendorSales();
-return{data:{vendorData,usersWishListedSales}}
+export async function loadAllVendorSales() {
+  const vendorData = await getAllVendorSales();
+  return { data: { vendorData } };
+}
+export async function loadAllVendorSalesWithUsersWishListedSales(userID: unknown) {
+  const usersWishListedSales = await getVendorSalesWithUsersWishListedSales(userID);
+  const vendorData = await getAllVendorSales();
+  return { data: { vendorData, usersWishListedSales } };
 }
