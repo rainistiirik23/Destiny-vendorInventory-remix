10d8ebe7e5be544751f57b888f203ef7a1cfaa6c
rainistiirik23 <rainis.tiirik9@gmail.com>
2026-03-31 12:13:22 +0300

    html structure changes and class additions

--- app/components/wishListedItems.tsx
+++ app/components/wishListedItems.tsx
@@ -13,34 +13,43 @@ export default function WishListedItems(data: wishListedVendorSalesProps) {
   }
   return (
     <div>
-      <ul>
+      <ul className="user-wishlisted-items-unordered-list">
         {usersWishListedSales.map((userWishListItem: userWishListItem, itemIndex: number) => {
           const itemPerks: perkObject = JSON.parse(userWishListItem.perks as string);
           const itemMasterworks: masterworkObjectArray = JSON.parse(userWishListItem.masterworks as string);
           return (
-            <div key={`item${itemIndex}`}>
-              <li className="user-item-info-container">
-                <img src={`http://www.bungie.net${userWishListItem.item_icon}`} alt="" />
-                <h1>{userWishListItem.item_name}</h1>
+            <li key={`item${itemIndex}`}>
+              <div className="user-item-info-container">
+                <div className="item-info-container">
+                  <img className="user-item-icon" src={`http://www.bungie.net${userWishListItem.item_icon}`} alt="" />
+                  <h1 className="user-item-header">{userWishListItem.item_name}</h1>
+                </div>
+                <h2 className="user-wishlisted-perks-header">Perks</h2>
                 <ul className="user-item-perks-masterworks-info-container">
                   {Object.keys(itemPerks).map((perkColumnKey, perkColumnIndex) => {
                     return (
-                      <li key={`item${itemIndex}-perkColumn${perkColumnKey}`}>
-                        <h2>{`Perk column ${perkColumnIndex + 1}`}</h2>
-                        <ul className="perks-unordered-list">
-                          {itemPerks[perkColumnKey].map((perk, perkIndex: number) => {
-                            return (
-                              <li key={`item${itemIndex}-perkColumn${perkColumnKey}-perk-${perkIndex}`}>
-                                <img
-                                  style={{ backgroundColor: "blue" }}
-                                  src={`http://www.bungie.net${perk.perkIcon}`}
-                                  alt=""
-                                />
-                                <span>{perk.perkName}</span>
-                              </li>
-                            );
-                          })}
-                        </ul>
+                      <li
+                        className="user-perks-unordered-list-item"
+                        key={`item${itemIndex}-perkColumn${perkColumnKey}`}
+                      >
+                        {/*    <h2 className="perk-column-header">{`Perk column ${perkColumnIndex + 1}`}</h2> */}
+                        {/*  <ul className="user-perks-unordered-list"> */}
+                        {itemPerks[perkColumnKey].map((perk, perkIndex: number) => {
+                          return (
+                            <div key={`item${itemIndex}-perkColumn${perkColumnKey}-perk-${perkIndex}`}>
+                              <img
+                                className="user-wishlisted-perk-icon"
+                                src={`http://www.bungie.net${perk.perkIcon}`}
+                                alt=""
+                              />
+                              {/* <div className="user-perk-description-container">
+                                <span className="user-perk-name">{perk.perkName}</span>
+                                  <p>{perk.perkDescription}</p> 
+                              </div> */}
+                            </div>
+                          );
+                        })}
+                        {/*  </ul> */}
                       </li>
                     );
                   })}
@@ -48,42 +57,59 @@ export default function WishListedItems(data: wishListedVendorSalesProps) {
                 <ul className="user-item-masterworks-info-container">
                   {itemMasterworks.map((masterwork, masterworkIndex: number) => {
                     return (
-                      <li key={`item${itemIndex}-masterwork-${masterworkIndex}`}>
-                        <img src={`http://www.bungie.net${masterwork.masterWorkIcon}`} alt="" />
-                        <span>{masterwork.masterWorkName}</span>
+                      <li
+                        className="user-item-masterworks-info-list-item"
+                        key={`item${itemIndex}-masterwork-${masterworkIndex}`}
+                      >
+                        <h2 className="user-item-masterworks-header">Masterworks</h2>
+                        <div className="user-item-masterworks-info-list-item-container">
+                          <img
+                            className="user-item-masterwork-icon"
+                            src={`http://www.bungie.net${masterwork.masterWorkIcon}`}
+                            alt=""
+                          />
+                          <span className="user-item-masterwork-name">{masterwork.masterWorkName}</span>
+                        </div>
                       </li>
                     );
                   })}
                 </ul>
-              </li>
-              <button
-                onClick={() => {
-                  console.log(userWishListItem);
-                  setWishlistedItemEditStateFunction(userWishListItem);
-                  setWishListedItemsComponentState("edit-wishlisted-item");
-                }}
-              >
-                Edit
-              </button>
-              <button
-                onClick={() => {
-                  setWishListedItemsComponentState("delete-wishlisted-item");
-                  setSelectedItemForDeletionFunction(userWishListItem);
-                }}
-              >
-                Delete
-              </button>
-            </div>
+              </div>
+              <div className="edit-delete-wishlisted-item-buttons-container">
+                <button
+                  className="edit-wishlisted-item-button"
+                  onClick={() => {
+                    console.log(userWishListItem);
+                    setWishlistedItemEditStateFunction(userWishListItem);
+                    setWishListedItemsComponentState("edit-wishlisted-item");
+                  }}
+                >
+                  Edit
+                </button>
+                <button
+                  className="delete-wishlisted-item-button"
+                  onClick={() => {
+                    setWishListedItemsComponentState("delete-wishlisted-item");
+                    setSelectedItemForDeletionFunction(userWishListItem);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </li>
           );
         })}
       </ul>
-      <button
-        onClick={() => {
-          setWishListedItemsComponentState("create-wishlisted-items");
-        }}
-      >
-        Add
-      </button>
+      <div className="wishlist-an-item-button-container">
+        <button
+          className="wishlist-an-item-button"
+          onClick={() => {
+            setWishListedItemsComponentState("create-wishlisted-items");
+          }}
+        >
+          Add
+        </button>
+      </div>
     </div>
   );
 }
